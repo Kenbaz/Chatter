6cbc690a2ccc6b3f171e3b18e29dfb7fd4f3299e
Kenbaz <kbassey016@gmail.com>
2024-08-23 00:06:10 +0100

    style adjustments

--- app/_components/ProfileEdit.tsx
+++ app/_components/ProfileEdit.tsx
@@ -265,7 +265,7 @@ const EditProfile: FC = () => {
   if (isLoading) return <div>Loading...</div>;
 
   return (
-    <div className="profile-edit-container mt-[70px] pb-16 text-tinWhite">
+    <div className="profile-edit-container mt-[70px] pb-[5rem] text-tinWhite">
       {error && <p className="text-red-600">{error}</p>}
       {successMessage && <p className="text-green-600">{successMessage}</p>}
 
--- app/_components/ProfilePage.tsx
+++ app/_components/ProfilePage.tsx
@@ -163,7 +163,7 @@ const ProfilePage: FC = () => {
   const isCurrentUser = user && user.uid === params.userId;
 
   return (
-    <div className="user-profile-page mt-[70px] h-auto pb-14 bg-primary">
+    <div className="user-profile-page mt-[70px] h-auto pb-16 bg-primary">
       {error && <p className="text-red-600">{error}</p>}
 
       <div className="profile-header pb-4 p-2 mt-5 relative dark:bg-primary mb-4">
